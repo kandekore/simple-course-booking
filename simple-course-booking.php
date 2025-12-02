368a4f5b190678f955f2f153b8852c88ea242f93
Kandekore <darren@kandekore.net>
2025-12-02 08:21:00 +0000

    assets route correction

--- simple-course-booking.php
+++ simple-course-booking.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Simple Course Booking
  * Description: Custom booking plugin with slots, attendees, and Zoom delivery.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: D Kandekore  
  */
 
